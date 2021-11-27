f4594dda1a37e1b215bec2d32668900dbd64ce74
siddhaerth <83398730+siddhaerth@users.noreply.github.com>
2021-11-27 15:22:12 +0530

    Add files via upload

---
+++ letschatroom.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAM090ezQ-2Zscv4XVXDwT77DTjYDiYVEE",
+    authDomain: "letschat-app-7341e.firebaseapp.com",
+    databaseURL: "https://letschat-app-7341e-default-rtdb.firebaseio.com",
+    projectId: "letschat-app-7341e",
+    storageBucket: "letschat-app-7341e.appspot.com",
+    messagingSenderId: "211479852799",
+    appId: "1:211479852799:web:6ae439d28d1208f6ce3eca"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
